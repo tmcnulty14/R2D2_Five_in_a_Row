300b349d154a702051bcab1c25fe98a3baf1cb39
tmcnulty14 <tmcnulty@student.framingham.edu>
2014-11-04 16:12:55 -0500

    Added GameController classes.
    Also cleaned up interface documentation.

--- ClientGUI.java
+++ ClientGUI.java
@@ -1,12 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- *
- * @author mcnultyt
- */
 public interface ClientGUI {
     /**
      * Updates the GUI's displayed board pieces.
---
+++ R2D2GameController.java
@@ -0,0 +1,9 @@
+/**
+ *
+ * @author tmcnulty14
+ */
+public class R2D2GameController implements GameController, Runnable {
+    public void run() {
+        
+    }
+}
