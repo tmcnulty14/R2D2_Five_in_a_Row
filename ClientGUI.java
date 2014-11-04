a784b2f01a1dd6b728b661f320bd669bed78356b
tmcnulty14 <tmcnulty@student.framingham.edu>
2014-11-04 15:46:47 -0500

    Fleshed out the interfaces.
    Added comments describing what the methods should do.

--- ClientGUI.java
+++ ClientGUI.java
@@ -8,5 +8,15 @@
  * @author mcnultyt
  */
 public interface ClientGUI {
+    /**
+     * Updates the GUI's displayed board pieces.
+     * @param board A 2d array of (0, 1, 2), 0 = empty, 1/2 = player 1/2
+     */
+    public void updateBoard(int[][] board);
     
+    /**
+     * Changes the message the GUI is displaying.
+     * @param message A message to be displayed on the GUI.
+     */
+    public void displayMessage(String message);
 }
--- ClientModel.java
+++ ClientModel.java
@@ -8,5 +8,10 @@
  * @author mcnultyt
  */
 public interface ClientModel {
+    /**
+     * Called by the GUI when the player clicks a board location.
+     * @param x The x-coordinate of the clicked location.
+     * @param y The y-coordinate of the clicked location.
+     */
     public void requestMove(int x, int y);
 }
