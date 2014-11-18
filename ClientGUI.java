653a9abb284162755f2813c0204778c117fe0b6a
tmcnulty14 <tmcnulty@student.framingham.edu>
2014-11-18 17:09:49 -0500

    Updated ClientGUI interface.

--- ClientGUI.java
+++ ClientGUI.java
@@ -1,9 +1,11 @@
 public interface ClientGUI {
     /**
-     * Updates the GUI's displayed board pieces.
-     * @param board A 2d array of (0, 1, 2), 0 = empty, 1/2 = player 1/2
+     * Adds a board piece to the GUI display.
+     * @param x
+     * @param y
+     * @param player 1 for player 1, 2 for player 2, 0 for empty.
      */
-    public void updateBoard(int[][] board);
+    public void updateBoard(int x, int y, int player);
     
     /**
      * Changes the message the GUI is displaying.
