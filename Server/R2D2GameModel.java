ce23017591133e9c724a235f63e6421a32d8b71d
Anthony Morla <anthonymorla@yahoo.com>
2014-12-10 17:50:36 -0500

    Comments updated

--- Server/R2D2GameModel.java
+++ Server/R2D2GameModel.java
@@ -1,9 +1,10 @@
 package Server;
 
 /**
- * The server class for the Five in a row game. This class handles
- * connecting to clients and spawning game instances for each pair of clients.
- * @author anthonymorl
+ * This game model class handles core game functions such as tracking
+ * current player, initializing the board array and storing, as well as 
+ * determining winning conditions and the associated winner
+ * @author AnthonyMorla
  */
 
 public class R2D2GameModel implements GameModel
