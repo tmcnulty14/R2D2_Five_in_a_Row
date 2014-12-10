4fe31dc951787e3b364a3f3017078371e7208cfa
wgalloway <wesleegalloway@gmail.com>
2014-12-10 13:21:32 -0500

    Update ClientGUI.java
    added more comments

--- Client/ClientGUI.java
+++ Client/ClientGUI.java
@@ -5,7 +5,8 @@ public interface ClientGUI {
      * Adds a board piece to the GUI display.
      * @param x
      * @param y
-     * @param player 1 for player 1, 2 for player 2, 0 for empty.
+     * 1 for player 1, 2 for player 2, 0 for empty.
+     * abstract class so other classes can inherit the methods
      */
     public void updateBoard(int x, int y, int player);
     
